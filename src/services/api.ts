479195d032e817135e1dda16167cd37bb6e5fec2
Shariful Islam <eng.mdshariful.islam.7@gmail.com>
2026-06-13 08:20:35 +0600

    sdsds

--- src/services/api.ts
+++ src/services/api.ts
@@ -3,8 +3,8 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 export const baseApi = createApi({
   reducerPath: 'api',
   baseQuery: fetchBaseQuery({
-    // baseUrl: 'https://api.linkfastesim.com/api/v1',
-    baseUrl: 'http://10.10.7.9:5000/api/v1',
+    baseUrl: 'https://api.linkfastesim.com/api/v1',
+    // baseUrl: 'http://10.10.7.9:5000/api/v1',
 
     prepareHeaders: (headers) => {
       const token = localStorage.getItem('token');
@@ -33,8 +33,8 @@ export const baseApi = createApi({
 });
 
 
-// export const imageUrl = 'https://api.linkfastesim.com';
-export const imageUrl = 'http://10.10.7.9:5000';
+export const imageUrl = 'https://api.linkfastesim.com';
+// export const imageUrl = 'http://10.10.7.9:5000';
 
 export const getImageUrl = (path: string) => {
   if(path.startsWith('http')) {
